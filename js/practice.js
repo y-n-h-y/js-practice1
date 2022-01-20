ff875511115df99378b48eccd926e52388b0e4d9
吉野弘之 <yoshinohiroyuki@yoshinohiroyuki-no-MacBook-Pro.local>
2022-01-20 23:04:36 +0900

    変数と条件式

--- js/practice.js
+++ js/practice.js
@@ -1,1 +1,42 @@
-alert('Hellow World');
+// let hellow = 'Hellow';
+// let world = 'World';
+
+// alert(hellow + world);
+
+// alert(4 + 3);
+// alert(8 - 5);
+// alert(2 * 6);
+// alert(10 / 2);
+
+// let orange = 100;
+// let apple = 120;
+
+// if (orange < apple) {
+//   alert('みかんの値段がリンゴより安い');
+// } else if (orange == apple) {
+//   alert('みかんの値段とりんごの値段が等しい');
+// } else {
+//   alert('みかんの値段がリンゴより高い');
+// };
+
+
+// let max = 100;
+// let num = 1;
+// let count = 0;
+
+// while (num < max) {
+//   num = num * 2;
+//   count = count + 1
+// }
+
+// alert('2をかけ続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+
+let i;
+let num = 0;
+
+for (i = 1; i < 11; i++) {
+  num = num + i;
+};
+
+alert('1から10まで足し算した結果は' + num + 'です');
