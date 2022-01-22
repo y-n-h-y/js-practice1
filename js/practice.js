cea8cebcb2e503062eb09baa7f66195a460a9734
吉野弘之 <yoshinohiroyuki@yoshinohiroyuki-no-MacBook-Pro.local>
2022-01-22 23:05:10 +0900

    変数宣言

--- js/practice.js
+++ js/practice.js
@@ -32,11 +32,61 @@
 // alert('2をかけ続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
 
 
-let i;
-let num = 0;
+// let i;
+// let num = 0;
 
-for (i = 1; i < 11; i++) {
-  num = num + i;
+// for (i = 1; i < 11; i++) {
+//   num = num + i;
+// };
+
+// alert('1から10まで足し算した結果は' + num + 'です');
+
+// const nicknam = 'イチロー';
+// console.log(nicknam);
+
+// const nicknam = 'タロー';
+// console.log(nicknam);
+// let nicknam = 'イチロー';
+// console.log(nicknam);
+// nicknam = 'ジロー';
+// console.log(nicknam);
+
+
+// var str = "webcamp";//グローバルスコープ
+
+// function foo() {
+//   console.log(str);
+//   var y = "hello";//関数スコープ
+// };
+
+// foo();
+// console.log(y);
+
+
+// function foo() {
+//   let x = "webcamp";
+//   {
+//     let y = "hello webcamp";
+//     console.log(y);
+//   };
+
+//   console.log(x);
+// };
+
+// foo();
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// };
+// console.log(i);
+
+
+var str = "webcamp";
+
+function foo() {
+  console.log(str);
+  var str = "dmm webcamp";
+  console.log(str);
 };
 
-alert('1から10まで足し算した結果は' + num + 'です');
+foo();
