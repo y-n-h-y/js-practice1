c042cbce50ac954481bf19ef451ad196a6f431ba
吉野弘之 <yoshinohiroyuki@yoshinohiroyuki-no-MacBook-Pro.local>
2022-01-26 23:48:24 +0900

    practice1 finish

--- js/practice.js
+++ js/practice.js
@@ -81,12 +81,83 @@
 // console.log(i);
 
 
-var str = "webcamp";
+// var str = "webcamp";
 
-function foo() {
-  console.log(str);
-  var str = "dmm webcamp";
-  console.log(str);
+// function foo() {
+//   console.log(str);
+//   var str = "dmm webcamp";
+//   console.log(str);
+// };
+
+// foo();
+
+// let alertString;
+// alertString = addString("webcamp");
+
+// alert(alertString);
+
+// function addString(strA) {
+//   let addStr = "Hello" + strA;
+//   return addStr;
+// };
+
+
+// let promptstar = prompt("好きな文字を入力してください");
+
+// alert(promptstar);
+
+
+let user_hand = prompt("ジャンケンの手をグー、チョキ、パーから選んでください。");
+
+let js_hand = getJShand();
+
+let judge = winLose(user_hand, js_hand);
+
+alert("あなたの選んだ手は、" + user_hand + "です。JavaScriptの選んだ手は" + js_hand + "です。結果は" + judge + "です。");
+
+function getJShand() {
+  let js_hand_num = Math.floor(Math.random() * 3);
+  let hand_name;
+
+  if (js_hand_num == 0) {
+    hand_name = "グー";
+  } else if (js_hand_num == 1) {
+    hand_name = "チョキ";
+  } else if (js_hand_num == 2) {
+    hand_name = "パー";
+  };
+
+  return hand_name;
 };
 
-foo();
+function winLose(user, js) {
+  let winLoseStr;
+
+  if (user == "グー") {
+    if (js == "グー") {
+      winLoseStr = "あいこ";
+    } else if (js == "チョキ") {
+      winLoseStr = "勝ち!";
+    } else if (js == "パー") {
+      winLoseStr = "負け、、";
+    };
+  } else if (user == "チョキ") {
+    if (js == "グー") {
+      winLoseStr = "負け、、";
+    } else if (js == "チョキ") {
+      winLoseStr = "あいこ";
+    } else if (js == "パー") {
+      winLoseStr = "勝ち!";
+    };
+  } else if (user == "パー") {
+    if (js == "グー") {
+      winLoseStr = "勝ち!";
+    } else if (js == "チョキ") {
+      winLoseStr = "負け、、";
+    } else if (js == "パー") {
+      winLoseStr = "あいこ";
+    };
+  };
+
+  return winLoseStr;
+};
