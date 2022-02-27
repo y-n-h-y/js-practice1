7a9ca87458f8b6b5b10957de9903c610a2aa5bc3
吉野弘之 <yoshinohiroyuki@yoshinohiroyuki-no-MacBook-Pro.local>
2022-02-27 23:13:22 +0900

    jquery

---
+++ js/jq-practice.js
@@ -0,0 +1,12 @@
+// $(document).ready(function () {
+//   $('body').html('<h1>Hello jQuery!</h1>')
+// })
+
+// $(function () {
+//   $('.box1').css({
+//     'background-color': '#0000FF',
+//     'height': '100px'
+//   });
+//   $('.box1').slideDown();
+//   $('.box1').slideUp();
+// });
--- js/practice.js
+++ js/practice.js
@@ -107,57 +107,57 @@
 // alert(promptstar);
 
 
-let user_hand = prompt("ジャンケンの手をグー、チョキ、パーから選んでください。");
+// let user_hand = prompt("ジャンケンの手をグー、チョキ、パーから選んでください。");
 
-let js_hand = getJShand();
+// let js_hand = getJShand();
 
-let judge = winLose(user_hand, js_hand);
+// let judge = winLose(user_hand, js_hand);
 
-alert("あなたの選んだ手は、" + user_hand + "です。JavaScriptの選んだ手は" + js_hand + "です。結果は" + judge + "です。");
+// alert("あなたの選んだ手は、" + user_hand + "です。JavaScriptの選んだ手は" + js_hand + "です。結果は" + judge + "です。");
 
-function getJShand() {
-  let js_hand_num = Math.floor(Math.random() * 3);
-  let hand_name;
+// function getJShand() {
+//   let js_hand_num = Math.floor(Math.random() * 3);
+//   let hand_name;
 
-  if (js_hand_num == 0) {
-    hand_name = "グー";
-  } else if (js_hand_num == 1) {
-    hand_name = "チョキ";
-  } else if (js_hand_num == 2) {
-    hand_name = "パー";
-  };
+//   if (js_hand_num == 0) {
+//     hand_name = "グー";
+//   } else if (js_hand_num == 1) {
+//     hand_name = "チョキ";
+//   } else if (js_hand_num == 2) {
+//     hand_name = "パー";
+//   };
 
-  return hand_name;
-};
+//   return hand_name;
+// };
 
-function winLose(user, js) {
-  let winLoseStr;
+// function winLose(user, js) {
+//   let winLoseStr;
 
-  if (user == "グー") {
-    if (js == "グー") {
-      winLoseStr = "あいこ";
-    } else if (js == "チョキ") {
-      winLoseStr = "勝ち!";
-    } else if (js == "パー") {
-      winLoseStr = "負け、、";
-    };
-  } else if (user == "チョキ") {
-    if (js == "グー") {
-      winLoseStr = "負け、、";
-    } else if (js == "チョキ") {
-      winLoseStr = "あいこ";
-    } else if (js == "パー") {
-      winLoseStr = "勝ち!";
-    };
-  } else if (user == "パー") {
-    if (js == "グー") {
-      winLoseStr = "勝ち!";
-    } else if (js == "チョキ") {
-      winLoseStr = "負け、、";
-    } else if (js == "パー") {
-      winLoseStr = "あいこ";
-    };
-  };
+//   if (user == "グー") {
+//     if (js == "グー") {
+//       winLoseStr = "あいこ";
+//     } else if (js == "チョキ") {
+//       winLoseStr = "勝ち!";
+//     } else if (js == "パー") {
+//       winLoseStr = "負け、、";
+//     };
+//   } else if (user == "チョキ") {
+//     if (js == "グー") {
+//       winLoseStr = "負け、、";
+//     } else if (js == "チョキ") {
+//       winLoseStr = "あいこ";
+//     } else if (js == "パー") {
+//       winLoseStr = "勝ち!";
+//     };
+//   } else if (user == "パー") {
+//     if (js == "グー") {
+//       winLoseStr = "勝ち!";
+//     } else if (js == "チョキ") {
+//       winLoseStr = "負け、、";
+//     } else if (js == "パー") {
+//       winLoseStr = "あいこ";
+//     };
+//   };
 
-  return winLoseStr;
-};
+//   return winLoseStr;
+// };
