13038f70ff58af546f948174b67eb8ab86c9ad28
吉野弘之 <yoshinohiroyuki@yoshinohiroyuki-no-MacBook-Pro.local>
2022-03-01 00:05:45 +0900

    ホバー クリックアクション

--- js/jq-practice.js
+++ js/jq-practice.js
@@ -10,3 +10,21 @@
 //   $('.box1').slideDown();
 //   $('.box1').slideUp();
 // });
+
+// $(function () {
+//   $('.box1').mouseover(function () {
+//     $('.box1').css({ 'background-color': '#0000ff' });
+//   });
+//   $('.box1').mouseout(function () {
+//     $('.box1').css({ 'background-color': '#ff0000' });
+//   });
+// });
+
+$(function () {
+  $('.box1').on('click', function () {
+    $('.box1').addClass('ext');
+  });
+  $('.box1').mouseout(function () {
+    $('.box1').removeClass('ext');
+  });
+});
